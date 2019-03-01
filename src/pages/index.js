c6500c5827c62943ce085f14ab2f46294234b89f
Dennis Morello <dennismorello@gmail.com>
2019-03-01 10:10:11 +0100

    Modified site title in SEO component

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title={siteTitle}
+          title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
