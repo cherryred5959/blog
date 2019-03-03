4ad144d2874901687e5b50dfb1cb2da8faa0cb6c
Dennis Morello <dennismorello@gmail.com>
2019-03-03 13:44:04 +0100

    Fixed wrong background width on post titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,7 @@ class BlogIndex extends React.Component {
                   backgroundColor: `#e71d73`,
                   fontFamily: `Montserrat, sans-serif`,
                   padding: `0.3em`,
-                  width: `fit-content`,
-                  width: `-moz-fit-content`,
+                  display: `table`
                 }}
               >
                 <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
