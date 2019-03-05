435592bdeadfd20e5b30dd22a85eb71e5627719d
Dennis Morello <dennismorello@gmail.com>
2019-03-06 00:16:06 +0100

    Moved Animate wrapper to Alert component

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Animate } from 'react-animate-mount';
 import styles from './alert.module.scss';
 
-export default props => <div className={styles.alert}>{props.message}</div>;
+export default props => (
+  <Animate show={props.show}>
+    <div className={styles.alert}>{props.message}</div>
+  </Animate>
+);
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Animate } from 'react-animate-mount';
 import styles from './layout.module.scss';
 
 import Alert from '../alert/alert';
@@ -60,9 +59,10 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
         }}
       >
-        <Animate show={this.state.isNewContentAvailable}>
-          <Alert message="New content is available, refresh the page 🎉" />
-        </Animate>
+        <Alert
+          show={this.state.isNewContentAvailable}
+          message="New content is available, refresh the page 🎉"
+        />
         <header>{header}</header>
         <main>{children}</main>
         <footer
