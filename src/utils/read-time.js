b183769f6f10efa163957808b5d817944e5d6a7b
Dennis Morello <dennismorello@gmail.com>
2019-03-11 23:37:15 +0100

    Simplified getReadTime

--- src/utils/read-time.js
+++ src/utils/read-time.js
@@ -1,9 +1,5 @@
 export const getReadTime = wordCount => {
-  const mins = Math.round(wordCount / 265);
+  const mins = Math.ceil(wordCount / 265);
 
-  if (mins <= 1) {
-    return `1 min read`;
-  } else {
-    return `${mins} mins read`;
-  }
+  return `${mins} min read`;
 };
