5c0fa22725a075c20c02ebe68d712b075d198bff
Dennis Morello <dennismorello@gmail.com>
2019-03-14 18:58:32 +0100

    Removed alert when service worker is registered

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import NewContentAvailable from './src/components/new-content-available/new-content-available';
-
 // Custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
@@ -10,16 +7,3 @@ import 'prism-themes/themes/prism-a11y-dark.css';
 
 // Global style
 import './src/styles/global.scss';
-
-// Handle new content availability
-export const onServiceWorkerUpdateReady = () => {
-  window.dispatchEvent(new Event('serviceWorkerUpdateReady'));
-};
-
-// Add new content available alert
-export const wrapPageElement = ({ element }) => (
-  <>
-    <NewContentAvailable />
-    {element}
-  </>
-);
